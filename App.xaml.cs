33bb0b64fbdf284db2f79395cb04da66c73c7e4c
笹谷 誠 <m.sasaya@gmail.com>
2026-04-21 23:06:48 +0900

    英語化対応準備

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,20 @@
 ﻿using System.Windows;
+#if false // trueにすると英語表記
+using System.Windows.Controls;
+using System.Globalization;
+using System.Threading;
+#endif
+
 
 namespace FlowInk;
 
 public partial class App : Application
 {
+#if false // trueにすると英語表記
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+        base.OnStartup(e);
+    }
+#endif
 }
